9b28d3a34a7e10c7208aae8397b660aade365d0a
sunrunhui <woaiios@sina.cn>
2017-03-14 10:12:26 +0800

    测试着玩的

---
+++ 测试/测试/ViewController.m
@@ -0,0 +1,30 @@
+//
+//  ViewController.m
+//  测试
+//
+//  Created by sun on 2017/3/14.
+//  Copyright © 2017年 xing. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+    NSLog(@"哈哈哈哈");
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
